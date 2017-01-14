2ec4dfee0f31953be8988e9e75ff95a06af1b9f1
Riku Lääkkölä <riku.laakkola@iki.fi>
2017-01-14 17:48:59 +0200

    initial put endpoint spaghetti

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,13 @@
 import flask_restful
 
 from app import app
-from views.routes import routes, RouteList
+from views.routes import routes, RouteList, RouteView
 from views.upload import upload, UploadView
 
 api = flask_restful.Api(app)
 api_root = '/api/v1'
 api.add_resource(RouteList, api_root + '/routes')
+api.add_resource(RouteView, api_root + '/routes/<string:id>')
 api.add_resource(UploadView, api_root + '/upload')
 
 app.register_blueprint(routes)
--- backend/views/routes.py
+++ backend/views/routes.py
@@ -1,6 +1,7 @@
 from flask_restful import marshal_with, fields, Resource
 from flask import Blueprint, request, abort, Response
 from datetime import datetime
+from bson.objectid import ObjectId
 import pymongo
 
 from common import InvalidUsage
@@ -50,3 +51,48 @@ class RouteList(Resource):
             [('expiration_date', pymongo.ASCENDING)]
         )
         return {'tickets': available_tickets}
+
+class RouteView(Resource):
+    def get(self, id):
+        return "Only PUT here!"
+
+    def put(self, id):
+        data = request.get_json(force=True)
+        reserved_arg = data.get('reserved')
+        used_arg = data.get('used')
+        if reserved_arg is None and used_arg is None:
+            abort(400)
+        ticket = mongo.db.tickets.find_one({
+            "_id": ObjectId(id),
+        })
+        if not ticket:
+            abort(204)
+
+        if reserved_arg is not None:
+            if reserved_arg and ticket.get('reserved') is not None:
+                abort(409)
+            if ticket.get('used') is not None:
+                abort(409)
+            reserved_state = datetime.now() if reserved_arg else None
+        else:
+            reserved_state = ticket.get('reserved')
+
+        if used_arg is not None:
+            if used_arg and ticket.get('used') is not None:
+                abort(409)
+            used_state = datetime.now() if used_arg else None
+        else:
+            used_state = ticket.get('used')
+
+        result = mongo.db.tickets.update_one(
+            {"_id": ObjectId(id)},
+            {
+                '$set': {
+                    "used": used_state,
+                    "reserved": reserved_state,
+                },
+            })
+        print result.matched_count
+        if result.matched_count < 1:
+            abort(400)
+        return (204)
