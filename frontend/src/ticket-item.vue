848ac4e52416c38de41d8394dc5bec0ea0f87bc5
Jari Porola <jari.porola@gofore.com>
2017-01-14 18:23:50 +0200

    reserve ticket

--- frontend/src/ticket-item.vue
+++ frontend/src/ticket-item.vue
@@ -4,6 +4,13 @@
     <span>{{ ticket.src }} - {{ ticket.dest }}</span><br />
     <span>{{ ticket.id }}</span><br />
     Voimassa: <span>{{ formatDate(ticket.expiration_date) }}</span>
+    <p>
+      <button v-on:click="reserveTicket(ticket.id)">Varaa lippu</button>
+    </p>
+    <p>
+      <span v-if="reservedOk">Varattu</span>
+      <span v-if="reservedNok">Ei onnistunut. Hae uusi lippu.</span>
+    </p>
   </div>
 </template>
 
@@ -15,10 +22,27 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      reservedOk: false,
+      reservedNok: false
+    }
+  },
   methods: {
     formatDate: function(timestamp) {
       var date = new Date(timestamp);
       return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
+    },
+    reserveTicket: function (id) {
+      this.$http.put('/api/v1/routes/' + id, {
+        reserved: true
+      }).then((response) => {
+        this.reservedOk = true;
+        this.reservedNok = false;
+      }, (response) => {
+        this.reservedOk = false;
+        this.reservedNok = true;
+      });
     }
   }
 }
